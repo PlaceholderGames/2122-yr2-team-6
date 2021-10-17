efb176ab12fd0896543b149e3301722dae755291
DinoPT <dinarte85@gmail.com>
2021-10-17 15:53:05 +0100

    door opening asset

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,22 +7,33 @@ public class playerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     public float runSpeed=40f;
+    
+    public Rigidbody2D rb;
 
     float HorizontalMove = 0f;
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Start()
     {
+   
         //Defining movement
-         HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+         rb = GetComponent <Rigidbody2D>();
         
     }
     
-    void FixedUpdate()
-    {
-        // Actually moving               Ensure mov is the same amount regardless of multiple calls
-        //                                                     Crouch Jump
-        controller.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
-        
+    // Update is called once per frame
+    private void Update()
+    {   if (Input.GetKeyDown("space") {
+            //Defining movement
+            rb.velocity = new Vector3(0, 14f, 0);
+        }
     }
+    
+    //private void FixedUpdate()
+    //{
+    //    // Actually moving               Ensure mov is the same amount regardless of multiple calls
+    //    //                                                     Crouch Jump
+    //    controller.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
+        
+    //}
 }
