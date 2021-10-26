783cf09c5c9394a336c933aa3f600c671c505296
DinoPT <dinarte85@gmail.com>
2021-10-26 17:29:26 +0100

    Health system working
    Although I am still trying to figure out how to pin it to the top left corner of the screen instead of the middle.

--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -14,11 +14,28 @@ public class playerHealth : MonoBehaviour
     
     void Update()
     {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
         for (int i = 0; i < hearts.Length; i++)
         {
-            if ()
+            if (i < health)
             {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i<numOfHearts)
+            {
+                hearts[i].enabled = true;
 
+            }
+            else
+            {
+                hearts[i].enabled = false;
             }
         }
     }
