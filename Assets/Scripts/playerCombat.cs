ae0f3e98406590f443f5ea07ed16670c7eae78d8
DinoPT <dinarte85@gmail.com>
2021-10-29 11:57:27 +0100

    Player atks WIP

--- Assets/Scripts/playerCombat.cs
+++ Assets/Scripts/playerCombat.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class playerCombat : MonoBehaviour
 {
+
+    public Animator animator;
+
+    public Transform attackPoint;
+
+    public float attackRange = 0.5f;
+
+    public LayerMask enemyLayers;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Attack();
         }
@@ -16,9 +25,23 @@ public class playerCombat : MonoBehaviour
     void Attack()
     {
         //Play ATK animation
+        animator.SetTrigger("Attack");
         //Detect Enemies in range of the atk
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         //Apply damage
-        
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            
+        }
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     void FixedUpdate()
