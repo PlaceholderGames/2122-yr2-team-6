70e9a6118237a8ec8f70d6f88b56e3b2603fedea
DinoPT <dinarte85@gmail.com>
2021-10-30 12:24:06 +0100

    DoorCollider with Level_trigger

--- Assets/Scripts/Level_trigger.cs
+++ Assets/Scripts/Level_trigger.cs
@@ -6,6 +6,9 @@ public class Level_trigger : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
